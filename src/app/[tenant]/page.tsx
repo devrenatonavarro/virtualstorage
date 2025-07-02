2fc2c4aa4d341a2ae6629c8708151a929993560a
Renato <satoshiune21@gmail.com>
2025-07-02 00:00:27 -0500

    first commit 1

--- src/app/[tenant]/page.tsx
+++ src/app/[tenant]/page.tsx
@@ -2,18 +2,20 @@ import { Metadata } from "next"
 import ClothingStore from "@/components/page/tenant/clothing-store"
 
 interface Props {
-  params: {
+  params: Promise<{
     tenant: string
-  }
+  }>
 }
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { tenant } = await params
   return {
-    title: `Tenant | ${params.tenant}`,
-    description: `Tenant: ${params.tenant}`,
+    title: `Tenant | ${tenant}`,
+    description: `Tenant: ${tenant}`,
   }
 }
 
-export default function page({ params }: Props) {
-  return <ClothingStore title={params.tenant} />
+export default async function page({ params }: Props) {
+  const { tenant } = await params
+  return <ClothingStore title={tenant} />
 }
